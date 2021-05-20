4217c19f8e7c3b77012cb927bf0c2c8bc24f62c5
LudovicAns <ludovicanselin@gmail.com>
2021-05-20 19:25:12 +0200

    Do some upgrade

--- monitoring.sh
+++ monitoring.sh
@@ -2,11 +2,11 @@
 
 TAB="	"
 IPv4=$(hostname -I | cut -d' ' -f1)
+ARCHITECTURE=$(uname -a)
 HOSTNAME=$(hostnamectl --static)
 USERNAME=$(whoami)
 TTY=$(ps hotty $$)
 EXEC_TIME=$(date "+%a %b %d %T %Y")
-ARCHITECTURE=$(uname -a)
 CPU=$(grep -c processor /proc/cpuinfo | cut -d' ' -f2)
 vCPU=$(grep 'cpu cores' /proc/cpuinfo | uniq | cut -d' ' -f3)
 USED_RAM=$(free -m | grep Mem | awk '{printf("%d", $3)}')
@@ -21,31 +21,38 @@ HOME_USED_SPACE=$(df /dev/mapper/crypt-home | grep /dev/mapper/crypt-home| awk '
 BOOT_USED_SPACE=$(df /dev/sda1 | grep /dev/sda1 | awk '{print $3}')
 USED_SPACE=$(echo $((${ROOT_USED_SPACE}+${HOME_USED_SPACE}+${BOOT_USED_SPACE})) | awk '{printf "%d", $1}')
 USED_SPACEP=$(echo ${TOTAL_SPACE} ${USED_SPACE} | awk '{printf "%.2f", ($2*100)/$1}')
-CPU_LOAD=$(top -n 1 | grep "%Cpu(s):" | awk '{printf "%.1f", $4}')
+CPU_LOAD_INT=$(top -n 1 | grep "%Cpu(s):" | awk '{printf "%d", $4}' | cut -d',' -f1)
+CPU_LOAD_FLOAT=$(top -n 1 | grep "%Cpu(s):" | awk '{printf "%d", $4}' | cut -d',' -f2)
 LAST_BOOT=$(who -b | grep "démarrage" | awk '{printf "%s %s", $3, $4}')
 LVM="no"
 TCP_COUNT=$(ss -s | grep TCP: | awk '{printf "%d", $4}')
 LOGGED_USERS=$(who | grep -c "")
 MAC=$(ip a | grep "link/ether" | awk '{print $2}')
-SUDO_CMD=$(sudo cat /var/log/sudo | grep -c COMMAND)
+SUDO_CMD=$(cat /var/log/sudo | grep -c COMMAND)
 
 if [ $(cat /etc/rpc | grep -c /dev/mapper/) ]
 then
 	LVM="yes"
 fi
 
+TMP="monitoring.tmp"
+rm -rf ${TMP}
+touch ${TMP}
+chmod 777 ${TMP}
 
-echo "Broadcast message from ${USERNAME}@${HOSTNAME} (${TTY}) (${EXEC_TIME}):"
-echo ""
-echo "${TAB}#Architecture: ${ARCHITECTURE}"
-echo "${TAB}#CPU physical: ${CPU}"
-echo "${TAB}#vCPU: ${vCPU}"
-echo "${TAB}#Memory Usage: ${USED_RAM}/${TOTAL_RAM}MB (${USED_RAMP}%)"
-echo "${TAB}#Disk Usage: ${USED_SPACE}/${TOTAL_SPACE}b (${USED_SPACEP}%)"
-echo "${TAB}#CPU Load: ${CPU_LOAD}%"
-echo "${TAB}#Last boot: ${LAST_BOOT}"
-echo "${TAB}#LVM use: ${LVM}"
-echo "${TAB}#Connexions TCP: ${TCP_COUNT} ETABLISHED"
-echo "${TAB}#User log: ${LOGGED_USERS}"
-echo "${TAB}#Network: IP ${IPv4} (${MAC})"
-echo "${TAB}#Sudo: ${SUDO_CMD} cmd"
+echo "Broadcast message from ${USERNAME}@${HOSTNAME} (${TTY}) (${EXEC_TIME}):" >> ${TMP}
+echo "" >> ${TMP}
+echo "${TAB}#Architecture: ${ARCHITECTURE}" >> ${TMP}
+echo "${TAB}#CPU physical: ${CPU}" >> ${TMP}
+echo "${TAB}#vCPU: ${vCPU}" >> ${TMP}
+echo "${TAB}#Memory Usage: ${USED_RAM}/${TOTAL_RAM}MB (${USED_RAMP}%)" >> ${TMP}
+echo "${TAB}#Disk Usage: ${USED_SPACE}/${TOTAL_SPACE}b (${USED_SPACEP}%)" >> ${TMP}
+echo "${TAB}#CPU Load: ${CPU_LOAD_INT}.${CPU_LOAD_FLOAT}%" >> ${TMP}
+echo "${TAB}#Last boot: ${LAST_BOOT}" >> ${TMP}
+echo "${TAB}#LVM use: ${LVM}" >> ${TMP}
+echo "${TAB}#Connexions TCP: ${TCP_COUNT} ETABLISHED" >> ${TMP}
+echo "${TAB}#User log: ${LOGGED_USERS}" >> ${TMP}
+echo "${TAB}#Network: IP ${IPv4} (${MAC})" >> ${TMP}
+echo "${TAB}#Sudo: ${SUDO_CMD} cmd" >> ${TMP}
+
+sudo wall -n ${TMP}
